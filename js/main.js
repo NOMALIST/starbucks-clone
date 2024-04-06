38dad048d34ff99616da9c8657c7c4408edbf87e
nomalist <yojun926@naver.com>
2024-04-07 00:26:20 +0900

    SECTION REWARD

--- js/main.js
+++ js/main.js
@@ -49,9 +49,44 @@ fadeEls.forEach((fedeEl, index) => {
   });
 });
 
-const swiper = new Swiper('.notice-line .swiper', {
+const swiper1 = new Swiper('.notice-line .swiper', {
   // Optional parameters
   direction: 'vertical',
   autoplay: true, // 자동 넘김
   loop: true, // 반복 여부
 });
+
+const swiper2 = new Swiper('.promotion .swiper', {
+  // Optional parameters
+  // direction: 'horizental'  디폴트 옵션
+  slidesPerView: 3,  // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true,
+  autoplay: {
+    delay : 5000
+  }, // 자동 넘김
+  loop: true, // 반복 여부
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next',
+  }
+});
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', () => {
+  isHidePromotion = !isHidePromotion;
+  if(!isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add('hide');
+  } else {
+    // 보이기 처리
+    promotionEl.classList.remove('hide');
+  }
+})
